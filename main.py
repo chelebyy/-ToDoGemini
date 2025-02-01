eed01fca78c6ecd5ed7bc26e0505d2e83138bbad
chelebyy <muhammett.celebi@gmail.com>
2025-02-01 19:19:14 +0300

    Veri Ekleme

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from alembic.util import status
 from fastapi import FastAPI, Depends, Path, HTTPException
+from numpy.core.defchararray import title
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from starlette import status
 from models import Base, Todo
@@ -9,6 +11,13 @@ from typing import Annotated
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
+
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=1000)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
 
 
 def get_db():
@@ -27,9 +36,20 @@ async def read_all(db: db_dependency):
     return db.query(Todo).all()
 
 
+# id ye göre filtreleme
+
 @app.get("/get_by_id/{todo_id}", status_code=status.HTTP_200_OK)
-async def read_by_id(db:db_dependency,todo_id: int =Path(gt=0)):
-    todo=db.query(Todo).filter(Todo.id==todo_id).first()
+async def read_by_id(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first()
     if todo is not None:
         return todo
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Todo not found")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+
+
+# Veri eklemek
+
+@app.post("/create_todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo = Todo(**todo_request.dict())
+    db.add(todo)
+    db.commit()
