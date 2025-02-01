310baebebba47f871425ee6752c8c58aad162c7a
chelebyy <muhammett.celebi@gmail.com>
2025-02-01 17:26:30 +0300

    Veritabanı Kolonlarına Karar Vermek

--- models.py
+++ models.py
@@ -1,1 +1,12 @@
+# Veritabanı Kolonlarına Karar Vermek
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean
 
+
+class Todo(Base):
+    __tablename__ = 'todos'
+    id=Column(Integer,primary_key=True,index=True)
+    title= Column(String)
+    description=Column(String)
+    priority=Column(Integer)
+    complete=Column(Boolean,default=False)
