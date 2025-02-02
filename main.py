ab16535698a9a0422b2e2ece2bc6ae026cfef436
chelebyy <muhammett.celebi@gmail.com>
2025-02-02 14:20:33 +0300

    Update and Delete İşlemleri

--- main.py
+++ main.py
@@ -53,3 +53,27 @@ async def create_todo(db: db_dependency, todo_request: TodoRequest):
     todo = Todo(**todo_request.dict())
     db.add(todo)
     db.commit()
+
+
+# Update ve Delete İşlemleri
+@app.put("/update_todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if todo is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+    todo.title = todo_request.title
+    todo.description = todo_request.description
+    todo.priority = todo_request.priority
+    todo.complete = todo_request.complete
+    db.add(todo)
+    db.commit()
+
+
+@app.delete("/delete_todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if todo is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+
+    db.delete((todo))
+    db.commit()
