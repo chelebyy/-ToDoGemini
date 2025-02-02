1eb3366d833633c644095a590fc724279c535b56
chelebyy <muhammett.celebi@gmail.com>
2025-02-02 15:28:22 +0300

    User Tablosu Oluşturuldu

--- models.py
+++ models.py
@@ -1,12 +1,26 @@
 # Veritabanı Kolonlarına Karar Vermek
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 
 
 class Todo(Base):
     __tablename__ = 'todos'
-    id=Column(Integer,primary_key=True,index=True)
-    title= Column(String)
-    description=Column(String)
-    priority=Column(Integer)
-    complete=Column(Boolean,default=False)
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey('users.id'))
+
+
+class User(Base):
+    __tablename__ = 'todos'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_Password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
